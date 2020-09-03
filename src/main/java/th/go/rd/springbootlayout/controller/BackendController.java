dffeea1078871105add6d2fd6771de8cfc041d68
immijunk <aim_junk@hotmail.com>
2020-09-03 16:12:24 +0700

    3/9/2563 3rd commit

--- src/main/java/th/go/rd/springbootlayout/controller/BackendController.java
+++ src/main/java/th/go/rd/springbootlayout/controller/BackendController.java
@@ -1,25 +1,52 @@
 package th.go.rd.springbootlayout.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import th.go.rd.springbootlayout.Service.StudentService;
+import th.go.rd.springbootlayout.model.Student;
 
 @Controller
 public class BackendController {
 
-    //Dashboard
+    @Autowired
+    private StudentService stdService;
+
+    // Dashboard
     @GetMapping(value = "/dashboard")
-    public String dashboard(){
+    public String dashboard() {
         return "backend/dashboard";
     }
 
     @GetMapping(value = "/student")
-    public String student(){
+    public String student() {
         return "backend/student";
     }
 
-    @GetMapping(value = "/addstudent")
-    public String addstudent(){
+    @GetMapping(value = "/newstudent")
+    public String newStudent(ModelMap model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
         return "backend/NewStudent";
+    }
+
+    @PostMapping("/savestudent")
+    public String saveStudent(
+        @Validated Student student,
+        BindingResult result,
+        RedirectAttributes redirectAttributes){
+            if(result.hasErrors()){
+                return "/newstudent";
+            }else{
+                stdService.save(student);
+                return "redirect:/student";
+            }
     }
 
 }
