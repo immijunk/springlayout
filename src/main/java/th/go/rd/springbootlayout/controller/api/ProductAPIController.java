d21e5e85e9874a2204bbd39dccdc8ca5a3a6fa4b
unknown <aim_junk@hotmail.com>
2020-09-24 14:13:54 +0700

    1

--- src/main/java/th/go/rd/springbootlayout/controller/api/ProductAPIController.java
+++ src/main/java/th/go/rd/springbootlayout/controller/api/ProductAPIController.java
@@ -1,14 +1,27 @@
 package th.go.rd.springbootlayout.controller.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import th.go.rd.springbootlayout.model.Product;
 import th.go.rd.springbootlayout.repository.ProductRepository;
 
 @RestController
+@RequestMapping("api")
 public class ProductAPIController {
     
     @Autowired
     private ProductRepository productRepository;
-    
+
+
+    //Read all product 
+    @GetMapping("/product")
+    public List<Product> findall(){
+        return productRepository.findAll();
+    }
+
 }
